68c7ef9fabf1c91188c65022120fa8ab61285fd0
Vinzenz Brantner <vinzenzbrantner@me.com>
2020-10-18 12:39:41 +0200

    add auth0-spa

--- src/main.js
+++ src/main.js
@@ -7,6 +7,25 @@ import { createProvider } from "./vue-apollo";
 
 Vue.config.productionTip = false;
 
+// Import the Auth0 configuration
+import { domain, clientId } from "../auth_config.json";
+
+// Import the plugin here
+import { Auth0Plugin } from "./auth";
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
+
 new Vue({
   router,
   store,
